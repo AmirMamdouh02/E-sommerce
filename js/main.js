19c0710da9cb3db33fa60a772a842026eea615b0
AmirMamdouh02 <amirmamdouhcis02@gmail.com>
2023-08-30 18:50:22 +0300

    PRoject1

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// var x = "ahmed"
+// var x = 10
+// var x = true
+// null
+// undefined
+// var userName = document.getElementById("demo")
+// userName.innerHTML = "Hello world"
+
+// window.alert("Hello User")
+// window.prompt("Enter your Name")
+// var x = 10;
+// if(x == 5){
+//     console.log("true")
+// }else if(x==11){
+// console.log("tmam")
+// }
+// else{
+//     console.log("false")
+// }
+// var numOne = Number(window.prompt("Enter Number 1"))
+// var numTwo = Number(window.prompt("Enter Number 2"))
+// var sum = numOne + numTwo
+// window.alert(sum);
+
+var x = 5;
+// while(x > 0){
+// console.log("true")
+// x--;
+// }
+
+// do{
+// console.log(x);
+// x++;
+// }while(x == 5)
+
+// for(var i = 0 ; i < 10 ; i++){
+// console.log(i);
+// }
+
+//         0          1     2    3     4    5
+var x = ["ahmed" , "mohamed",10 , 30 , 50 , 60]
+x.splice(-3)
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
